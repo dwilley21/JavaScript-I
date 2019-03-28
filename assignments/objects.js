8de094fb82a2a0d4b11ed2212abdc278ab656b00
dwilley21 <daltonwilley@gmail.com>
2019-03-27 22:04:07 -0500

    completed MVP SOMEHOW

--- assignments/objects.js
+++ assignments/objects.js
@@ -62,22 +62,22 @@ const Antonietta = {
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
-// Mitzi's name
-console.log(Mitzi["name"]);
-// Kennan's ID
-console.log(Kennan["id"])
-// Keven's email
-console.log(Keven["email"])
-// Gannie's name
-console.log(Gannie["name"])
-// Antonietta's Gender
-console.log(Antonietta["gender"]);
-// ==== Challenge 3: Object Methods ==== 
-// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-console.log(Kennan.speak());
+// // Mitzi's name
+// console.log(Mitzi["name"]);
+// // Kennan's ID
+// console.log(Kennan["id"])
+// // Keven's email
+// console.log(Keven["email"])
+// // Gannie's name
+// console.log(Gannie["name"])
+// // Antonietta's Gender
+// console.log(Antonietta["gender"]);
+// // ==== Challenge 3: Object Methods ==== 
+// // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+// console.log(Kennan.speak());
 
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-console.log(Antonietta.multiplyNums(3,4));
+// // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+// console.log(Antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
